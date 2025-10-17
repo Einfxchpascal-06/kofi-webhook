0b7f0665520988e82fe4d10d43ac8d3ae849ac38
Einfxchpascal-06 <pascal.leo.hobein@gmail.com>
2025-10-17 15:22:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
